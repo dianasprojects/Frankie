e34e033cecbe00c439326a9fd3711ac7bd1b7c04
dianasprojects <dde.dimitrova@gmail.com>
2026-01-30 15:10:42 +0100

    Update delete_log.py

--- Frankie/delete_log.py
+++ Frankie/delete_log.py
@@ -1,3 +1,7 @@
+# This file allows you to delete ALL the files stored on your Raspberry Pi (Rpi) which contain 'UTC.txt'
+# These are the logs you have created on your device, the code allows you to not have to delete
+# them manually. Run it in the Thonny terminal directly - it should not be saved on your Rpi.
+
 import os
 
 # Get all files in the current directory
